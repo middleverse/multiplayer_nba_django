1db38e790244c8714779f996959f0306f939e42a
middleverse <arshi_b@hotmail.com>
2020-12-17 19:12:16 -0500

    create court functionality added

--- core/urls.py
+++ core/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create', views.create, name='create'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html', {})
-    
+    return render(request, 'index.html')
+
+def create(request):
+    return render(request, 'create.html')
