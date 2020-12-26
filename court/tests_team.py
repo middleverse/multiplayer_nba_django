1595a29a3853f2825a3003bbe4e19baae2a703a0
middleverse <arshi_b@hotmail.com>
2020-12-26 10:45:42 -0500

    added skeleton for team model testing

---
+++ court/tests_team.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+
+# Create your tests here.
+from .models import Team
+
+class TeamTestCase(TestCase):
+    '''
+    Testing for Team Model
+    '''
+    def setUp(self):
+        pass
+
+    def test_team_created(self):
+        pass
+
+    def test_correct_team_name(self):
+        pass
+
+    def test_correct_division_name(self):
+        pass
+
+    def test_all_teams_present(self):
+        pass
+
+
+
